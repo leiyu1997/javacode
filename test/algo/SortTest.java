0f06c4ca328168324424d5dac542bdc72672b056
yuleif <yuleif@yonyou.com>
2020-09-19 15:41:02 +0800

    Javacode

--- test/algo/SortTest.java
+++ test/algo/SortTest.java
@@ -1,63 +1,33 @@
 package algo;
 
-import com.leiyu.algo.sort.sorts.*;
 import com.leiyu.algo.sort.utils.ArrayUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 /**
  * 排序算法测试类
  */
 public class SortTest {
 
-    ParentSort pSort;
     int[] array;
+    long beginTime;
+
+    @Test
+    public void sort() {
+
+    }
 
     @Before
     public void before() {
-        int length = 1000000;
-        array = ArrayUtils.getArray(length);
-    }
-
-    @Test//默认排序
-    public void parentSortTest() {
-        pSort = new ParentSort();
-    }
-
-    @Test//选择排序测试
-    public void selectionTest() {
-        pSort = new Selection();
-    }
-
-    @Test//插入排序测试
-    public void insertionTest() {
-        pSort = new Insertion();
-    }
-
-    @Test//冒泡排序
-    public void bubbleTest() {
-        pSort = new Bubble();
-    }
-
-    @Test//堆排序
-    public void heapTest() {
-        pSort = new Heap();
-    }
-
-    @Test//快速排序
-    public void quickTest() {
-        pSort = new Quick();
+        array = ArrayUtils.getArray(1000);
+        beginTime = System.currentTimeMillis();
     }
 
     @After
     public void after() {
-        long beginTime = System.currentTimeMillis();
-        pSort.sort(array);
         long time = System.currentTimeMillis() - beginTime;
 //        System.out.println(pSort.getName() + "results:" + Arrays.toString(array));
-        System.out.println(String.format("%s\tisSuccess:%b,\ttime(ms): %d", pSort.getName(), ArrayUtils.isSort(array), time));
+        System.out.println(String.format("isSuccess:%b,\ttime(ms): %d", ArrayUtils.isSort(array), time));
     }
 }
