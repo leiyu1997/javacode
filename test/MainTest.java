c49bdfd2df9673ce24b3554383997160375cfa7d
yuleif <yuleif@yonyou.com>
2020-10-13 21:02:34 +0800

    Javacode

--- test/MainTest.java
+++ test/MainTest.java
@@ -10,6 +10,24 @@ public class MainTest {
 
     @Test
     public void test() {
-        System.out.println(1 / 2);
+        int[] array = {1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 7, 8, 8, 9, 9, 10, 11, 12, 13, 14, 15, 16};
+        int pos = 0, len = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1, tempPos = i, tempLen = 0; j < array.length; j++) {
+                i = j - 1;
+                if (array[j] - array[j - 1] == 1) {
+                    tempLen++;
+                    if (tempLen > len) {
+                        len = tempLen;
+                        pos = tempPos;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        for (int i = pos; i <= pos + len; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
